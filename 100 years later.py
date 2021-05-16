fc7393b22deecd0d8b182b189057c2b351766191
aryanSmalhotra <malhotra.singh.aryan@gmail.com>
2021-05-17 01:43:53 +0530

    100 years later

---
+++ 100 years later.py
@@ -0,0 +1,6 @@
+#in this program it will tell in wich year your will turn 100
+
+name = input("what is your name")
+user_age = int(input("what is your age"))
+year = 2021 - user_age + 100
+print("Hello",name,"you will turn 100 in this year",year)
