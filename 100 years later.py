9cfef6be6f1480bf157e750129871f1ac1fed51b
aryanSmalhotra <malhotra.singh.aryan@gmail.com>
2021-05-17 20:15:59 +0530

    hello this is Aryan

--- 100 years later.py
+++ 100 years later.py
@@ -1,4 +1,4 @@
-#in this program it will tell in wich year your will turn 100
+#in this program it will tell in wich year your will turn 100 
 
 name = input("what is your name")
 user_age = int(input("what is your age"))
